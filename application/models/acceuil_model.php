cbb04adde6da37e8b1516bc299922c65ca2722b7
unknown <13094@JAONA-IAM.VIVETIC>
2019-04-05 14:44:40 +0300

    deuxieme changement

--- application/models/acceuil_model.php
+++ application/models/acceuil_model.php
@@ -7,8 +7,10 @@ class Acceuil_model extends CI_Model {
 
         $vendu = $this->db->query("SELECT COALESCE(c.designation_produit,p.designation_produit) as produit, p.prix_unitaire ,p.quantite,
       COALESCE(c.quantite_vendu , 0) as quantite_vendu ,
-      COALESCE(p.quantite-c.quantite_vendu, p.quantite) as reste
-    FROM [wallboard].[dbo].[produit] p full outer join [wallboard].[dbo].[commande] c on p.designation_produit = c.designation_produit ");
+      COALESCE(p.quantite-c.quantite_vendu, p.quantite) as reste,
+      COALESCE(p.prix_unitaire*c.quantite_vendu,0) as montant,
+      cl.nom_client
+    FROM [wallboard].[dbo].[produit] p full outer join [wallboard].[dbo].[commande] c on p.designation_produit = c.designation_produit full outer join [wallboard].[dbo].[client] cl on cl.matricule_client = c.matricule_client ");
         $vendus = $vendu->result();
         //var_dump($vendus);die();
          return $vendus;
--- application/views/page/dashboard/content.php
+++ application/views/page/dashboard/content.php
@@ -5,7 +5,9 @@
                 <th scope="col">Produit</th>
                 <th scope="col">Stock</th>
                 <th scope="col">Vendu</th>
-                <th scope="col">Reste</th>               
+                <th scope="col">Montant</th>
+                <th scope="col">Reste</th>     
+                <th scope="col">Client</th>                         
             </tr>
         </thead>
         <?php if( isset($valeur) && count($valeur) ): ?>
@@ -15,7 +17,10 @@
                 <td><?php echo $resultat->produit;?></td>
                 <td><?php echo $resultat->quantite;?></td>
                 <td><?php echo $resultat->quantite_vendu;?></td>
-                <td><?php echo $resultat->reste;?></td>                  
+                <td><?php echo $resultat->montant;?></td>
+                <td><?php echo $resultat->reste;?></td>   
+                <td><?php echo $resultat->nom_client;?></td>   
+                               
             </tr> 
             </tbody>
          <?php endforeach;?>
