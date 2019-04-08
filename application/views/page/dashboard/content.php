43c37ed07d650dbbddfa43b86e15037bd02e384a
unknown <13094@JAONA-IAM.VIVETIC>
2019-04-08 10:12:40 +0300

    ajout total card

--- application/views/page/dashboard/content.php
+++ application/views/page/dashboard/content.php
@@ -1,3 +1,19 @@
+<style>
+.card{
+    width : 200px;
+    text-align : center;
+}
+.card:hover{
+    background-color : #C3D7D7;
+    width : 210px;
+    height : 110px;
+}
+
+</style>
+
+
+
+
 <div class="container">
     <table class="table table-sm">
         <thead class="thead-dark">
@@ -6,7 +22,7 @@
                 <th scope="col">Stock</th>
                 <th scope="col">Vendu</th>
                 <th scope="col">Montant</th>
-                <th scope="col">Reste</th>     
+                <th scope="col">Reste</th>   
             </tr>
         </thead>
         <?php if( isset($valeur) && count($valeur) ): ?>
@@ -18,14 +34,67 @@
                 <td><?php echo $resultat->quantite_vendu;?></td>
                 <td><?php echo $resultat->montant;?></td>
                 <td><?php echo $resultat->reste;?></td>   
-                               
             </tr> 
             </tbody>
+    
+            
          <?php endforeach;?>
         <?php endif;?>
+
+        
     </table>
+
+    <?php        
+        $total_stock = 0;
+        $total_vendu = 0;
+        $total_montant = 0;
+        $total_reste = 0;
+        foreach ($valeur as $value): 
+            $total_stock += $value->quantite;
+            $total_vendu += $value->quantite_vendu;
+            $total_montant += $value->montant;
+            $total_reste += $value->reste; 
+            
+        endforeach;
+
+    ?>    
+
+        <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+                <div class="card" style="">
+                    <div class="card-body">
+                    <h5 class="card-title">Total Stock</h5>
+                    <p class="card-text"><?php echo $total_stock;?></p>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card" style="">
+                    <div class="card-body">
+                    <h5 class="card-title">Total Vendu</h5>
+                    <p class="card-text"><?php echo $total_vendu;?></p>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card" style="">
+                    <div class="card-body">
+                    <h5 class="card-title"> Montant Total</h5>
+                    <p class="card-text"><?php echo $total_montant;?> Ariary</p>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card" style="">
+                    <div class="card-body">
+                    <h5 class="card-title">reste stock</h5>
+                    <p class="card-text"><?php echo $total_reste;?></p>
+                </div>
+                </div>
+            </div>
+        </div>          
+        </div>
+        
+
 </div>
-
-
-
-
